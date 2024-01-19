0b7d1d9aacb12f78cdb467dc455650580b8ebc00
SanketDhabarde <sanketdhabarde13@gmail.com>
2024-01-19 23:45:25 +0530

    feat: add route [GET] /api/videos/:id

--- handlers/video.handler.js
+++ handlers/video.handler.js
@@ -6,8 +6,19 @@ const getAllVideos = async (req, res) => {
     res.status(200).json({ total: videos.length, videos });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Something went wrong" });
+    res.status(500).json({ message: "Something went wrong!" });
   }
 };
 
-module.exports = { getAllVideos };
+const getVideo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const video = await Video.findById(id);
+    res.status(200).json({ video });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong!" });
+  }
+};
+
+module.exports = { getAllVideos, getVideo };
--- routes/video.router.js
+++ routes/video.router.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { getAllVideos } = require("../handlers/video.handler");
+const { getAllVideos, getVideo } = require("../handlers/video.handler");
 
 router.route("/").get(getAllVideos);
+router.route("/:id").get(getVideo);
 
 module.exports = router;
