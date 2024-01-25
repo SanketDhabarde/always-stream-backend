85c420f1d621a6c740903fdc45cae2f3cc36f81e
SanketDhabarde <sanketdhabarde13@gmail.com>
2024-01-26 01:02:44 +0530

    feat: add error handler middleware

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const routes = require("./routes");
 const { initializeDbConnection } = require("./db/db.connect");
 const { routeNotFound } = require("./middlewares/route-not-found.middleware");
+const { errorHandler } = require("./middlewares/error-handler.middleware");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,6 +20,7 @@ app.get("/", (req, res) => {
 });
 
 app.use(routeNotFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`app started at port ${port}`);
